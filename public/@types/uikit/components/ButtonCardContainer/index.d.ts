14e7541d9066df851aa0f102648605319ff63562
Nikita Green <nikita.green@soft-fx.lv>
2022-09-26 13:14:13 +0300

    deleted types

--- public/@types/uikit/components/ButtonCardContainer/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface IButtonCardContainer {
-    children: any;
-    sx?: any;
-}
-declare const ButtonCardContainer: React.FC<IButtonCardContainer>;
-export { ButtonCardContainer };
--- public/@types/uikit/components/ThemeSwitch/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface IThemeSwitch {
-    isDarkTheme: boolean;
-    onChange: () => void;
-}
-declare const ThemeSwitch: ({ isDarkTheme, onChange }: IThemeSwitch) => JSX.Element;
-export { ThemeSwitch };
