268ea3c6f6e6699a757784e9f25e1993d4f53e9d
Artur <artur.ahmadulin@softnet.com>
2022-10-14 11:44:25 +0300

    #1203093245499373: Onboarding - Address - Нет анимации появления селекта выбора города

--- src/components/CustomPopperAutocomplete/index.tsx
+++ src/components/CustomPopperAutocomplete/index.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
+
 import { Grow, Popper, PopperProps } from '@mui/material'
 
 export const CustromPopperAutocomplete = ({ children, ...rest }: PopperProps) => (
   <Popper placement="bottom-start" transition {...rest}>
     {({ TransitionProps }) => (
       <Grow in={rest.open} {...TransitionProps}>
-        {children}
+        {children as React.ReactElement}
       </Grow>
     )}
   </Popper>
