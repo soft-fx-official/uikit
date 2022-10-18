9e0731bf991e9800b6bccb0716f968cd9e5fb9e1
Artur <artur.ahmadulin@softnet.com>
2022-10-18 13:29:31 +0300

    #1203174978391324: Onboarding - Доработка Autocomplete

--- src/components/CustomAutocompleteCountries/index.tsx
+++ src/components/CustomAutocompleteCountries/index.tsx
@@ -1,161 +1,64 @@
-import { useEffect, useState } from 'react'
 import ReactCountryFlag from 'react-country-flag'
 import { capitalize } from 'lodash'
 
-import {
-  Autocomplete,
-  AutocompleteProps,
-  Box,
-  Grow,
-  InputAdornment,
-  Popper,
-  PopperProps,
-  TextField,
-  Typography,
-} from '@mui/material'
+import { Autocomplete, AutocompleteProps, Box, InputAdornment, TextField } from '@mui/material'
 
+import { CustromPopperAutocomplete } from '../CustomPopperAutocomplete'
 import { CustomTooltip as Tooltip } from '../CustomTooltip'
 import styles from './index.module.css'
 
-interface CustomAutocompleteCountriesProps extends AutocompleteProps {
-  options: any[]
-  isLoading?: boolean
+export interface CustomAutocompleteCountriesProps<T>
+  extends Omit<AutocompleteProps<T, boolean, true, boolean, React.ElementType>, 'renderInput'> {
   errorMessage?: string
-  onChange: (value: any) => void
-  value: any
   label?: string
   placeholder?: string
-  withoutTooltip?: boolean
-  type?: 'country' | 'phone' | 'nationality'
 }
 
-const CustomPopper = ({ children, ...rest }: PopperProps) => (
-  <Popper placement="bottom-start" transition {...rest}>
-    {({ TransitionProps }) => (
-      <Grow in={rest.open} {...TransitionProps}>
-        {children}
-      </Grow>
-    )}
-  </Popper>
-)
-
-export const CustomAutocompleteCountries: React.FC<CustomAutocompleteCountriesProps> = ({
-  options,
-  isLoading,
+export const CustomAutocompleteCountries: React.FC<CustomAutocompleteCountriesProps<any[]>> = ({
   errorMessage,
-  onChange,
-  value,
   label,
   placeholder,
-  withoutTooltip = false,
-  type,
   ...rest
 }) => {
   const hasError = Boolean(errorMessage)
-  const [phoneValue, setPhoneValue] = useState('')
-  const [open, setOpen] = useState(false)
-
-  const getOptionValue = (option: { phoneCode: string; name: string }) =>
-    `${option.phoneCode}                                   ${option.name}`
-
-  const beuteNumber = (number: string): string => {
-    if (/[0-9]/.test(number)) {
-      return number.slice(0, number.search(/\d\D*$/) + 1)
-    }
-    return number
-  }
-
-  useEffect(() => {
-    if (type === 'phone') {
-      if (value) setPhoneValue(value.phoneCode)
-      if (open) setPhoneValue('')
-    }
-  }, [type, value, open])
 
   return (
-    <Tooltip
-      title={errorMessage || ''}
-      arrow
-      open={hasError}
-      placement="right"
-      isDisabled={withoutTooltip}
-    >
+    <Tooltip title={errorMessage || ''} arrow open={hasError} placement="right">
       <Autocomplete
-        open={open}
-        onOpen={() => {
-          setOpen(true)
-          setPhoneValue('')
-        }}
-        onClose={() => setOpen(false)}
-        fullWidth
-        options={options}
-        onChange={(_, value) => {
-          onChange(value)
-        }}
-        isOptionEqualToValue={(option, value) => option.code === value.code}
+        PopperComponent={CustromPopperAutocomplete}
         autoHighlight
-        getOptionLabel={option =>
-          type === 'phone' ? getOptionValue(option) : capitalize(option.name)
-        }
-        inputValue={type === 'phone' ? phoneValue : undefined}
-        onInputChange={(_, inputValue) => {
-          if (inputValue && type === 'phone') setPhoneValue(beuteNumber(inputValue))
-        }}
-        renderOption={(props, option) => {
-          let countryName = capitalize(option.name)
-          countryName = countryName.length > 17 ? `${countryName.substring(0, 17)}...` : countryName
-
-          return (
-            <Box component="li" className={styles.option} {...props}>
-              <ReactCountryFlag
-                countryCode={option.code || option.countryCode}
-                svg
-                className={styles.flag}
-              />
-              <Box className={styles.countryName}>{countryName}</Box>
-              {option.phoneCode && (
-                <Typography className={styles.phoneCode} color="secondary" variant="caption">
-                  {option.phoneCode}
-                </Typography>
-              )}
-            </Box>
-          )
-        }}
-        componentsProps={{
-          paper: {
-            sx: theme => ({
-              width: type === 'phone' ? 300 : 'auto',
-              boxShadow: `3px 0 0 0 ${theme.palette.background.paper}, -3px 0 0 0 ${theme.palette.background.paper}`,
-              margin: '10px 0',
-            }),
-          },
-        }}
-        loading={isLoading}
-        PopperComponent={CustomPopper}
+        getOptionLabel={option => capitalize(option.name)}
+        renderOption={(props, option) => (
+          <Box component="li" className={styles.option} {...props}>
+            <ReactCountryFlag
+              countryCode={option.code || option.countryCode}
+              svg
+              className={styles.flag}
+            />
+            <Box className={styles.optionName}>{capitalize(option.name)}</Box>
+          </Box>
+        )}
         renderInput={params => (
           <TextField
             {...params}
-            name={type}
-            label={type === 'phone' ? undefined : label}
+            name="country"
+            label={label}
             placeholder={placeholder}
             variant="filled"
-            color={hasError ? 'warning' : value && 'success'}
+            color={hasError ? 'warning' : rest.value && 'success'}
             error={hasError}
             InputProps={{
               ...params.InputProps,
-
               inputProps: params.inputProps,
-              classes: { root: type === 'phone' ? styles.inputRoot : '' },
-              startAdornment: value && (
+              startAdornment: rest.value && (
                 <InputAdornment position="start" className={styles.inputAdornment}>
                   <ReactCountryFlag
-                    countryCode={value.code || value.countryCode || value}
+                    countryCode={rest.value.countryCode || rest.value}
                     svg
                     className={styles.flag}
                   />
                 </InputAdornment>
               ),
-              autoComplete: 'new-password', // disable autocomplete and autofill
             }}
           />
         )}
